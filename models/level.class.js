0b10f2af298cdf9fefa12e1d1ecb421674874418
s4mastig <maximilian.stigler@gmx.net>
2024-08-30 15:36:53 +0200

    character position and level border adjusted

--- models/level.class.js
+++ models/level.class.js
@@ -2,10 +2,11 @@ class Level {
     enemies;
     clouds;
     backgroundObjects;
+    level_end_x = 700;
 
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;
         this.clouds = clouds;
-        this. backgroundObjects = backgroundObjects;
+        this.backgroundObjects = backgroundObjects;
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,9 +1,7 @@
 class World {
 
     character = new Character();
-    enemies = level1.enemies;
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1;
     ctx;
     canvas;
     keyboard;
@@ -26,10 +24,10 @@ class World {
 
         this.ctx.translate(this.camera_x, 0)
         
-        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.clouds);
 
         
         this.ctx.translate(-this.camera_x, 0)
